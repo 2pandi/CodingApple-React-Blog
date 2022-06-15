f039ba31ecad76c36d88f964828832bb628777df
2pandi <coder.2pandi@gmail.com>
2022-06-15 13:13:58 +0900

    JS, CSS: nav-bar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let postTitle = '송파 초밥 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>2pandi의 블로그</h4>
+      </div>
+      <h4>{postTitle}</h4>
     </div>
   );
 }
